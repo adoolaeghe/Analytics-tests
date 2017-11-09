35215ae4ddf7abc9e41009360e781edb01e776ff
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-11-09 18:10:14 +0000

    It can buy a new share in a layer

--- echomancy/models/layer.js
+++ echomancy/models/layer.js
@@ -1,7 +1,8 @@
 var Share = require('./share.js')
 
 function Layer(shares) {
-  this.shares = []
+  this.shares = [];
+  this.sharesAvailable = shares
   for (i = 0; i < shares; i++) {
     this.generateShare()
   };
@@ -11,5 +12,10 @@ Layer.prototype.generateShare = function() {
   share = new Share
   this.shares.push(share)
 };
+
+Layer.prototype.buyShare = function() {
+  this.shares[this.sharesAvailable - 1].buy()
+  this.sharesAvailable -= 1
+}
 
 module.exports = Layer
--- echomancy/test/layer-test.js
+++ echomancy/test/layer-test.js
@@ -3,10 +3,14 @@ var chai = require('chai')
 var expect = chai.expect
 var Layer = require('../models/layer')
 
-
 describe('Layer', function() {
   it('initialise with a given price', function() {
     var layer = new Layer(4)
     expect(layer.shares.length).to.equal(4);
   });
+  it('buy the last share in the layer', function() {
+    var layer = new Layer(4)
+    layer.buyShare()
+    expect(layer.sharesAvailable).to.equal(3)
+  })
 });
