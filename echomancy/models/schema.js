9987f935bd1b4405f15a2f01001279e16c19e074
Antoine Doolaeghe <antoinedoolaeghe@gmail.com>
2017-11-10 17:42:22 +0000

    added multiply number of shares for each layer

--- echomancy/models/schema.js
+++ echomancy/models/schema.js
@@ -1,19 +1,24 @@
 var Layer = require("./layer.js");
 
-function Schema(price, incrementRate) {
+function Schema(price, incrementRate, multiplier, initialShares) {
   this.currentPrice = price
   this.incrementRate = incrementRate
-  this.layers = [new Layer(4, 1)];
+  this.multiplier = multiplier
+  this.layers = [new Layer(initialShares, 1)];
 }
 
 Schema.prototype.buy = function() {
   this.layers[this.layers.length - 1].buyShare();
   if (this.layers[this.layers.length - 1].checkLayerFull) {
-    this.layers.push(new Layer(4, this.currentPrice));
+    var newNumberOfShares = this.calculateShareNumber()
+    this.layers.push(new Layer(newNumberOfShares, this.currentPrice));
     this.currentPrice = this.layers.length * this.incrementRate
   }
 };
 
+Schema.prototype.calculateShareNumber = function() {
+  return this.layers[this.layers.length - 1].shares.length * this.multiplier
+}
 
 
 module.exports = Schema;
--- echomancy/test/schema-test.js
+++ echomancy/test/schema-test.js
@@ -15,24 +15,31 @@ describe("Schema", function() {
   });
 
   it("buys a share when share is available", function() {
-    var schema = new Schema(1, 1.1);
+    var schema = new Schema(1, 1.1, 1, 4);
     schema.buy();
     expect(schema.layers[0].shares[schema.layers[0].shares.length - 1].owned).to
       .be.true;
   });
 
   it("creates a new layer when there is no available share in the current layer", function() {
-    var schema = new Schema(1, 1.1);
+    var schema = new Schema(1, 1.1, 1, 4);
     for (i = 0; i < 4; i++) {
       schema.buy();
     }
     expect(schema.layers.length).to.equal(2);
   });
   it("increment price when creating a new Layer", function() {
-    var schema = new Schema(1, 1.1);
+    var schema = new Schema(1, 1.1, 1, 4);
     for (i = 0; i < 4; i++) {
       schema.buy();
     }
     expect(schema.currentPrice).to.equal(2.2)
+  })
+  it("creates new layer with a number of share multiplied by as variable", function() {
+    var schema = new Schema(1, 1.1, 4, 4)
+    for (i = 0; i < 4; i++) {
+      schema.buy();
+    }
+    expect(schema.layers[schema.layers.length - 1].shares.length).to.equal(16)
   })
 });
